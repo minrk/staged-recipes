8f9a44cd52697f5bbaa502bc1c8508e5517f783f
Min RK <benjaminrk@gmail.com>
2016-09-06 15:12:20 +0200

    bump openmpi to 2.0.1
    include sedinplace patches, default options from https://bitbucket.org/petsc/conda-recipes

--- recipes/openmpi/build.sh
+++ recipes/openmpi/build.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-OPTS=""
+OPTS="--disable-dependency-tracking"
 if [[ $(uname) == Darwin ]]; then
-    OPTS="--disable-mpi-fortran"
+    OPTS="$OPTS --disable-mpi-fortran"
 fi
 
 ./configure --prefix=$PREFIX $OPTS
+
+# Patches from https://bitbucket.org/petsc/conda-recipes
+sedinplace() { [[ $(uname) == Darwin ]] && sed -i "" $@ || sed -i"" $@; }
+sedinplace s%--prefix=$PREFIX%--prefix=\$PREFIX%g opal/include/opal_config.h
+sedinplace s%-I$(dirname $SRC_DIR)%-I%g           opal/include/opal_config.h
+sedinplace /-DOMPI_MSGQ_DLL=/d                    ompi/debuggers/Makefile
+
 make
 make check
 make install
